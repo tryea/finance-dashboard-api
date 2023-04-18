585e6809992e4836399910f085162e4e39aff5c1
aristoersapta@gmail.com <aristoersapta@gmail.com>
2023-04-18 09:39:26 +0700

    [main] add products api

--- routes/product.js
+++ routes/product.js
@@ -6,8 +6,8 @@ const productRoutes = express.Router()
 
 productRoutes.get('/products', async (req, res) => {
     try {
-        const kpis = await Product.find()
-        res.status(200).json(kpis)
+        const products = await Product.find()
+        res.status(200).json(products)
     } catch (err) {
         res.status(404).json({ message: err.message })
     }
